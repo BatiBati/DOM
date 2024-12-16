b3afa29d9f7dab3111d535d9bd7114634ead8774
BatiBati <94386708+BatiBati@users.noreply.github.com>
2024-12-16 18:20:00 +0800

    DOM lesson

---
+++ index.js
@@ -0,0 +1,69 @@
+// const parent = document.getElementById("parentDiv");
+// const p_tag = document.createElement("p");
+// p_tag.innerHTML = "Created P tag";
+
+// document.getElementById("parentDiv").appendChild(p_tag);
+
+// const new_element = document.createElement("div");
+// new_element.innerHTML = "Replaced element";
+
+// document.getElementById("parentDiv").replaceChild(new_element, p_tag);
+
+// new_element.style.color = "green";
+// new_element.style.fontSize = "60px";
+
+// const input = document.createElement("input");
+// document.getElementById("parentDiv").appendChild(input);
+
+// new_element.addEventListener("mouseenter", () => {
+//   new_element.style.color = "red";
+// });
+// new_element.addEventListener("mouseout", () => {
+//   new_element.style.color = "green";
+// });
+// new_element.addEventListener("click", () => {
+//   console.log("clicked");
+// });
+//--------------//--------------//--------------//--------------//----
+/* const loginForm = document.getElementById("Login");
+loginForm.style.display = "flex";
+loginForm.style.flexDirection = "column";
+loginForm.style.justifyContent = "center";
+loginForm.style.alignItems = "center";
+const pTag = document.createElement("p");
+pTag.innerHTML = "Login";
+pTag.style.fontWeight = "700";
+pTag.style.textAlign = "center";
+pTag.style.fontSize = "30px";
+document.getElementById("Login").appendChild(pTag);
+//------------Email Input -----------------------------------------
+const inputEmail = document.createElement("Input");
+
+inputEmail.style.margin = "10px" ;
+inputEmail.style.width = "200px";
+document.getElementById("Login").appendChild(inputEmail);
+//------------Password Input -----------------------------------------
+const inputPassword = document.createElement("Input");
+
+inputPassword.style.margin = "10px";
+inputPassword.style.width = "200px";
+document.getElementById("Login").appendChild(inputPassword);
+//------------Login Submit -------------------------------------------
+
+const loginButton = document.createElement("button")
+const p_login = document.createElement("p")
+p_login.style.padding = "20px"; */
+
+//--------------//--------------//--------------//--------------//----
+document.body.style.width = "100px";
+document.body.style.textAlign = "center";
+
+const loginTitle = document.createElement("p")
+loginTitle.innerHTML = "Login";
+loginTitle.style.fontSize = "60px";
+loginTitle.style.fontWeight = "Bold";
+document.getElementById("parentContainer").appendChild(login)
+
+const emailTitle = document.createElement("p");
+emailTitle = document 
+
